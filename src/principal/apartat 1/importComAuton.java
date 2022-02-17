7cd79d89f89d7eaebf47810aaceb243dbc8d304d
Admartgar <94359780+Admartgar@users.noreply.github.com>
2022-02-17 13:15:40 +0100

    Add files via upload

---
+++ src/principal/apartat 1/importComAuton.java
@@ -0,0 +1,108 @@
+package projecte_base_dades.activitat;
+
+import java.sql.*;
+import java.util.Calendar;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.*;
+
+public class importComAuton {
+
+
+    static int codiComunAuto,codiProvinciaINE;
+    static String nom;
+    static int comunAutoId = 1;
+
+    static void introducir(){
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            Connection con=DriverManager.getConnection("jdbc:mysql://192.168.56.102:3306/Eleccions_Generals_GrupB","perepi","pastanaga");
+
+
+            //Preparem el Date
+            Calendar calendar = Calendar.getInstance();
+            java.sql.Date startDate = new java.sql.Date(calendar.getTime().getTime());
+
+            // the mysql insert statement
+            String query = " INSERT INTO comunitats_autonomes (comunitat_aut_id,nom,codi_ine)"
+                    + " values (?,?,?)";
+
+            // create the mysql insert preparedstatement
+            PreparedStatement preparedStmt = con.prepareStatement(query);
+            preparedStmt.setInt(1,comunAutoId);
+            preparedStmt.setString (2, nom);
+            preparedStmt.setInt (3, codiComunAuto);
+
+
+
+
+            // execute the preparedstatement
+            preparedStmt.execute();
+
+            //Tanquem la connexió
+            con.close();
+        }
+        catch(Exception e){
+            System.out.println(e);
+        }
+    }
+
+
+
+    public static void importar(){
+        BufferedReader bfLector = null;
+        try {
+
+            //Obtenim el directori actual
+            Path pathActual = Paths.get(System.getProperty("user.dir"));
+
+            //Concatenem el directori actual amb un subdirectori "dades" i afegim el fitxer "03021911.DAT"
+            Path pathFitxer = Paths.get(pathActual.toString(), "temp", "07021904.DAT");
+
+            //objReader = new BufferedReader(new FileReader(pathFitxer.toString()));
+
+            bfLector = Files.newBufferedReader(pathFitxer, StandardCharsets.ISO_8859_1);
+            String strLinia;
+            while ((strLinia = bfLector.readLine()) != null) {
+                separar(strLinia);
+                if (codiProvinciaINE == 99 && codiComunAuto != 99) {
+                    introducir();
+                    comunAutoId++;
+                }
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (bfLector != null)
+                    bfLector.close();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+
+
+
+    static void separar(String linea){
+        codiComunAuto = stringAInt(linea.substring(9,11));
+        codiProvinciaINE = stringAInt(linea.substring(11,13));
+        nom = linea.substring(14,64).trim();
+
+    }
+
+    static int stringAInt (String s) {
+        int x;
+        try {
+            x = Integer.parseInt(s);
+        } catch (NumberFormatException e) {
+            x = 0;
+        }
+        return x;
+    }
+
+}
