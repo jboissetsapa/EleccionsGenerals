ad4bc5cb91c80c3cc9500c27bc56f74dbc05732c
Emma <emsaal3597@gmail.com>
2022-02-27 18:07:28 +0100

    llei d'hondt

--- src/projecte_base_dades/apartat1/ImportarCandidats.java
+++ src/projecte_base_dades/apartat1/ImportarCandidats.java
@@ -1,5 +1,5 @@
 package projecte_base_dades.apartat1;
-
+import projecte_base_dades.apartat4.DescomprimirZip;
 import java.io.BufferedReader;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
---
+++ src/projecte_base_dades/apartat5/lleidHondt.java
@@ -0,0 +1,60 @@
+package projecte_base_dades.apartat5;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.Locale;
+
+public class lleidHondt {
+    public static void resultatProvincies(String nomProvincia) {
+
+        try {
+
+
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            Connection con = DriverManager.getConnection("jdbc:mysql://192.168.75.129/Eleccions_Generals_GrupB", "perepi", "pastanaga");
+
+
+            String query = "SELECT p.nom, sum(em.vots_emesos), sum(em.vots_blanc), sum(em.vots_nuls) FROM eleccions_municipis em INNER JOIN municipis m on m.municipi_id = em.municipi_id INNER JOIN provincies p  on m.provincia_id = p.provincia_id  GROUP BY p.nom HAVING p.nom = ?";
+
+
+            PreparedStatement preparedStmt = con.prepareStatement(query);
+            preparedStmt.setString(1, nomProvincia);
+            ResultSet rs = preparedStmt.executeQuery();
+
+            while (rs.next()) {
+                String nomProv = rs.getString(1);
+                int votsEmesos = rs.getInt(2);
+                int votsBlanc = rs.getInt(3);
+                int votsNuls = rs.getInt(4);
+                System.out.println("PROVINCIA " + nomProv.toUpperCase(Locale.ROOT));
+                System.out.println("----------------------");
+                System.out.println("Vots: " + votsEmesos);
+                System.out.println("Vots blanc: " + votsBlanc);
+                System.out.println("Vots nuls: " + votsNuls);
+
+
+            }
+
+            query = "SELECT c.nom_curt, vp.candidats_obtinguts FROM candidatures c INNER JOIN vots_candidatures_prov vp ON c.candidatura_id = vp.candidatura_id INNER JOIN provincies p ON p.provincia_id = vp.provincia_id WHERE p.nom = ? ORDER BY vp.candidats_obtinguts DESC";
+            preparedStmt = con.prepareStatement(query);
+            preparedStmt.setString(1, nomProvincia);
+            rs = preparedStmt.executeQuery();
+
+            while (rs.next()) {
+                String nomCandidautra = rs.getString(1);
+                int escons = rs.getInt(2);
+                System.out.println(nomCandidautra + ": " + escons);
+
+            }
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}
