cabc585422a0207f642b62cb3996e353e97d2617
Admartgar <94359780+Admartgar@users.noreply.github.com>
2022-02-28 00:04:30 +0100

    Add files via upload

---
+++ src/projecte_base_dades/ConnexioDBGrup2.java
@@ -0,0 +1,44 @@
+package projecte.sapa;
+
+import java.sql.*;
+
+public class ConnexioDBGrup2 {
+
+    // Propietats
+    private static Connection conn = null;
+    String driver = "com.mysql.cj.jdbc.Driver"; //com.mysql.jdbc.Driver
+    String url;
+    String usuari ="perepi";
+    String contrasenya = "pastanaga";
+    String host = "192.168.56.102";
+    String base_dades = "Eleccions_Generals_GrupB";
+
+    // Constructors
+    ConnexioDBGrup2(){
+
+        this.url = "jdbc:mysql://" + host + ":3306/" + base_dades;
+
+        try{
+            Class.forName(driver);
+            conn = DriverManager.getConnection(url, usuari, contrasenya);
+        }
+        catch(ClassNotFoundException | SQLException e){
+            e.printStackTrace();
+        }
+    }
+
+    // Mètodes
+    public static Connection getConnection() {
+
+        if (conn == null){
+            new ConnexioDBGrup2();
+        }
+        return conn;
+    }
+
+    public static void closeConnection() throws SQLException {
+        if (conn != null) {
+            conn.close();
+        }
+    }
+}
