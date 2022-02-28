4f6dbbf7a9f842a485b687449d09da36fae896ce
Admartgar <94359780+Admartgar@users.noreply.github.com>
2022-02-28 20:07:41 +0100

    Update ImportVotsAutonomic.java

--- src/projecte_base_dades/apartat1/ImportVotsAutonomic.java
+++ src/projecte_base_dades/apartat1/ImportVotsAutonomic.java
@@ -1,6 +1,6 @@
 package projecte_base_dades.apartat1;
-import projecte_base_dades.apartat4.DescomprimirZip;
 
+import projecte_base_dades.apartat4.DescomprimirZip;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
